0b10f64e44c13beb3773391265d4b3f1e9674e79
ZHANG DING <zding92@126.com>
2015-07-26 22:56:42 +0800

    修改login中判断是否用户存在、是否登陆成功等信息。修改register()以实现采用model方式的register功能

--- Application/Home/Controller/IndexController.class.php
+++ Application/Home/Controller/IndexController.class.php
@@ -8,33 +8,37 @@ class IndexController extends Controller {
     }
     
     public function login() {
-    	$Data = M('info');
+        $Data = D('info');
     	session_start();
     	$user = I('post.username', 0, 'strip_tags,htmlspecialchars,trim');
     	$password = I('post.password', 0, 'strip_tags,htmlspecialchars,trim');
-    	$condition['ID'] = $user;
+    	$condition['username'] = $user;
     	$result = $Data->where($condition)->field('PASSWORD,USERNAME')->find();
-
-    	if ($result['password'] == $password) {
+		if ($result == null){
+			echo 'var user_noexist=true;
+				  var login=false;';
+		}
+		else if ($result['password'] == md5($password)) {
 	    	echo "var login=true;
-	        	  var user_noexist=false;
-	        	  var pwd_error=false;";
-	     	session('user', $user);
-	     	session('nickname', $result['name']);
+        		  var user_noexist=false;
+        	  	  var pwd_error=false;";
+     		session('user', $user);
+     		session('nickname', $result['name']);
     	}
+    	else	echo "var login=false;
+        		      var user_noexist=false;
+        	  	      var pwd_error=true;";
     }
     
     public function register() {
     	$Data = D('info');
     	$password = I('post.password', 0, 'strip_tags,htmlspecialchars,trim');
     	if ($Data->create()) {
-    		echo "var user_noexist = false;";
-//     		$Data->add();
+     		$Data->add();
+     		echo 'var reg_success=true;';
     	} else {
-    		echo "var user_noexist = true;";
+    		echo 'var reg_success=false;';
     	}
     	
-    }
-    
-    
+    }   
 }
