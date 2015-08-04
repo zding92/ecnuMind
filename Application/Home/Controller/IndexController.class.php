a74ce419bdf4abe66566239df5320f438ba7ef5d
Tian Yu <tian_yu92@yahoo.com>
2015-08-04 14:19:13 +0800

    采取ajaxReturn代替echo返回js变量

--- Application/Home/Controller/IndexController.class.php
+++ Application/Home/Controller/IndexController.class.php
@@ -11,8 +11,6 @@ class IndexController extends Controller {
     
     /**
      * 登录按钮Ajax提交Target
-     * @return 我是一个返回值
-     * @param $parameter：我是一个参数
      */
     public function login() {
     	// 利用Model类，快速设置约束条件，并创建D对象
@@ -27,21 +25,23 @@ class IndexController extends Controller {
     	$result = $Data->where($condition)->field('PASSWORD,USERNAME')->find();
     	// 如果查询失败，代表user不存在，返回下列js变量，利用eval脚本解析器运行获取变量。
 		if ($result == null){
-			echo 'var user_noexist=true;
-				  var login=false;';
+			$this->ajaxReturn( "var user_noexist=true; var login=false;",
+					                     "EVAL");
 		}
 		// 如果查询成功，且密码正确，返回下列js变量，利用eval脚本解析器运行获取变量。
 		else if ($result['password'] == md5($password)) {
-	    	echo "var login=true;
-        		  var user_noexist=false;
-        	  	  var pwd_error=false;";
+	    	$this->ajaxReturn("var login=true;
+        		  						  var user_noexist=false;
+        	  	                          var pwd_error=false;",
+	    								"EVAL");
 	    	// 将username放入session，作为后续用户操作身份句柄
      		session('username', $username);
     	}
     	// 否则代表存在用户，但是密码错误，返回下列js变量，利用eval脚本解析器获取变量
-    	else	echo "var login=false;
-        		      var user_noexist=false;
-        	  	      var pwd_error=true;";
+    	else	$this->ajaxReturn("var login=false;
+        		     						  var user_noexist=false;
+        	  	      						  var pwd_error=true;",
+    			                            "EVAL");
     }
     
     /**
@@ -59,9 +59,9 @@ class IndexController extends Controller {
     		// 操作数据库->添加数据
      		$Data->add();
      		// 返回操作结果。利用js的eval脚本解析功能读取数据。
-     		echo 'var reg_success=true;';
+     		$this->ajaxReturn("var reg_success=true;","EVAL");
     	} else {
-    		echo 'var reg_success=false;';
+    		$this->ajaxReturn("var reg_success=false;","EVAL");
     	}
     	
     }   
