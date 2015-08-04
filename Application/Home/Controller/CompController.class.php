809fa286adf9f802c2857fecadce0a32f25b6144
ZHANG DING <zding92@126.com>
2015-08-04 10:56:37 +0800

    添加比赛报名页面（并未加入info_container)，表格显示（但是并未实现json数据读取显示）

---
+++ Application/Home/Controller/CompController.class.php
@@ -0,0 +1,8 @@
+<?php
+namespace Home\Controller;
+use Think\Controller;
+class CompController extends Controller {
+	public function Comp(){
+		$this->display();
+	}
+}
---
+++ Public/js/Comp/Comp.js
@@ -0,0 +1,36 @@
+/**
+ * 
+ */
+$(document).ready(function () {
+	alert('into the function');
+    $.ajax({
+        url: compJSON,
+        async: false,
+        success: function (result) {
+            $("tbody tr").detach();
+            eval(result);
+            alert('In to json ajax');
+            for (var i = 0; i < json_string.length; i++) {
+                if (json_string[i].link !== "")
+                    $("tbody").append("<tr><td data-title='ID'>" + json_string[i].no +
+                                        "</td><td data-title='Name'>" + json_string[i].name +
+                                        "</td><td data-title='Link'><a href='" + json_string[i].link + " 'target='_blank' >Link</a></td>" +
+                                        "</td><td data-title='Status'>" + json_string[i].status + "</td></tr>");
+                else
+                    $("tbody").append("<tr><td data-title='ID'>" + json_string[i].no +
+                                        "</td><td data-title='Name'>" + json_string[i].name +
+                                        "</td><td data-title='Link'>Link</td>" +
+                                        "</td><td data-title='Status'>" + json_string[i].status + "</td></tr>");
+
+            }
+
+        }
+    });
+
+    parent.document.getElementById("Comp_frame").height = document.body.scrollHeight;
+});
+
+$(document).ready(function(){
+    $("tbody tr").mouseenter(function(){$(this).css("background-color","#B3E5FC");});
+    $("tbody tr").mouseleave(function(){$(this).css("background-color","#fff");});
+}); 
