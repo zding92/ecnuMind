bcb1b656ad6d1a5ff840671a564a874d186b4690
ZHANG DING <zding92@126.com>
2015-08-04 13:30:22 +0800

    添加comp.js compcontroller.class.php中的注释

--- Application/Home/Controller/CompController.class.php
+++ Application/Home/Controller/CompController.class.php
@@ -3,6 +3,6 @@ namespace Home\Controller;
 use Think\Controller;
 class CompController extends Controller {
 	public function Comp(){
-		$this->display();
+		$this->display();//显示__app__/home/comp/comp/comp页面
 	}
 }
--- Public/js/Comp/Comp.js
+++ Public/js/Comp/Comp.js
@@ -1,5 +1,5 @@
 /**
- * 
+ * 取Comp.dat中的json数据，以在前台显示
  */
 $(document).ready(function () {
 	//alert('into the function');
@@ -8,15 +8,15 @@ $(document).ready(function () {
         async: false,
         success: function (result) {
         	//alert('In to json ajax');
-            $("tbody tr").detach();
+            $("tbody tr").detach();//先删去表格中原始的行
             eval(result);
-            for (var i = 0; i < json_string.length; i++) {
-                if (json_string[i].link !== "")
+            for (var i = 0; i < json_string.length; i++) { //遍历所有的json数组元素
+                if (json_string[i].link !== "")//当json元素有link项时，从json数组中读取数据并append推送到前台页面中
                     $("tbody").append("<tr><td data-title='ID'>" + json_string[i].no +
                                         "</td><td data-title='Name'>" + json_string[i].name +
                                         "</td><td data-title='Link'><a href='" + json_string[i].link + " 'target='_blank' >Link</a></td>" +
                                         "</td><td data-title='Status'>" + json_string[i].status + "</td></tr>");
-                else
+                else//当json元素无link项时，从json数组中读取数据并append推送到前台页面中
                     $("tbody").append("<tr><td data-title='ID'>" + json_string[i].no +
                                         "</td><td data-title='Name'>" + json_string[i].name +
                                         "</td><td data-title='Link'>Link</td>" +
@@ -26,8 +26,8 @@ $(document).ready(function () {
 
         }
     });    
-    $("tbody tr").mouseenter(function(){$(this).css("background-color","#B3E5FC");});
-    $("tbody tr").mouseleave(function(){$(this).css("background-color","#fff");});
-    parent.document.getElementById("Comp_frame").height = document.body.scrollHeight;
+    $("tbody tr").mouseenter(function(){$(this).css("background-color","#B3E5FC");});//当鼠标进入表格某行时，此行变蓝
+    $("tbody tr").mouseleave(function(){$(this).css("background-color","#fff");});	//当鼠标移出某行时，此行变白
+    parent.document.getElementById("Comp_frame").height = document.body.scrollHeight;//配合iframe显示
 });
 
