733ba795aa92bb990a2d5d57ddd720fafc385528
ZHANG DING <zding92@126.com>
2015-08-08 13:38:45 +0800

    添加Tiaozhan写入数据库函数

--- Application/Home/Controller/TiaozhanController.class.php
+++ Application/Home/Controller/TiaozhanController.class.php
@@ -2,8 +2,20 @@
 namespace Home\Controller;
 use Think\Controller;
 class TiaozhanController extends Controller {
-	public function Tiaozhan(){
+    public function Tiaozhan(){
 		//显示__app__/home/Tiaozhan/Tiaozhan页面
 		$this->display();
+	}
+	
+	/**
+	 * 将挑战杯的数据录入数据库
+	 */
+	public function TiaozhanAddData(){
+		//将comp_Tiaozhan数据表实例化
+		$Data = M('Tiaozhan','comp_');		
+		// 根据表单提交的POST数据创建数据对象
+		$Data->create();
+		// 根据条件保存修改的数据
+		if ($Data->save()) $this->ajaxReturn("var tiaozhanDataWri=true;","EVAL");; 	
 	}
 }
