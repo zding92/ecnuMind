6dd1e144ae2a8cd3a646fa66beba56e3e7b93fc2
chrisliu54 <chrisliu54@126.com>
2015-08-13 17:54:07 +0800

    将Home.html中的竞赛报名页面iframe换成了load方法

--- Public/js/home/User.js
+++ Public/js/home/User.js
@@ -372,10 +372,13 @@ var user_json;
 
     function CompHtml()
     {
-        $(".info_container").append("<iframe class='iframe' name='Comp_frame' id='Comp_frame'   \
+    	/*
+         $(".info_container").append("<iframe class='iframe' name='Comp_frame' id='Comp_frame'   \
          src= '" + app_url + "/Home/Comp/Comp" + "' seamless='seamless' scrolling='no'   \
          onload='this.height=Comp_frame.document.body.scrollHeight' \
-         onhaschange='this.height=Comp_frame.document.body.scrollHeight'></iframe>")                    
+         onhaschange='this.height=Comp_frame.document.body.scrollHeight'></iframe>")
+    	 */
+    	$(".info_container").load("../Comp/Comp.html");
     }
     
     function ProHtml() {
