30b8ebe00f77c8c2fe75a6962fe6dcd007859291
ZHANG DING <zding92@126.com>
2015-08-20 13:57:57 +0800

    完善jquery-ui中确实的images

--- Public/js/home/hoverTag.js
+++ Public/js/home/hoverTag.js
@@ -10,14 +10,7 @@ $(function() {
         if ( element.is( ".tabs p" ) ) {
           return "<b>"+tagText+"</b>"+"的获评分数：<br><div class='hoverTagStar'></div>"+"<b>"+tagText+"</b>"+"的获评次数：";
         }
-		$('.hoverTagStar').raty({ 
-			  number   : 10,
-			  score    : 10,
-			  starOff  : starOffIcon,
-			  starOn   : starOnIcon,
-			  starHalf : starHalfIcon,
-			  readOnly : true,		  
-	  	});
+		
       }
     });
 });
