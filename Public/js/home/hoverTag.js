dfe8a75c2c343bc207a9274ee94426f068af8df5
ZHANG DING <zding92@126.com>
2015-08-20 15:31:03 +0800

    完成个人主页浮动能力打分显示

--- Public/js/home/hoverTag.js
+++ Public/js/home/hoverTag.js
@@ -7,10 +7,22 @@ $(function() {
       content: function() {
         var element = $(this);
         var tagText = $(this).text();
+        var starScore = 8.5;
+        var scoreTimes = 66;
         if ( element.is( ".tabs p" ) ) {
-          return "<b>"+tagText+"</b>"+"的获评分数：<br><div class='hoverTagStar'></div>"+"<b>"+tagText+"</b>"+"的获评次数：";
-        }
-		
+          return "<b>"+tagText+"</b>"+"的获评分数：<br><div class='hoverTagStar'></div>" +
+                 starScore+"分<br>"+"<b>"+tagText+"</b>"+"的获评次数：" +scoreTimes+"次"+
+          		 "<script type='text/javascript'>" +
+          		 	"$('.hoverTagStar').raty({" + 
+          		 		"number   : 10,"+
+          		 		"score    :"+starScore+","+
+          		 		"starOff  : starOffIcon,"+
+          		 		"starOn   : starOnIcon,"+
+          		 		"starHalf : starHalfIcon,"+
+          		 		"readOnly : true,"+		  
+        				"});" +
+          		 "</script>";         
+        }	
       }
     });
 });
