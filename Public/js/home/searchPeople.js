a6296049d4225aa1de6a5539f0000376de3b089c
ZHANG DING <zding92@126.com>
2015-08-23 20:24:57 +0800

    修改搜人筛选池标签样式

--- Public/js/home/searchPeople.js
+++ Public/js/home/searchPeople.js
@@ -11,23 +11,26 @@ $(document).ready(function() {
 		/*take out the text of the tag just clicked*/
 		var clickedText = $(this).text();
 
+		var x=$($(this)).offset();
+    	alert("Top: " + x.top + " Left: " + x.left);
+
+
 		/*Judge each clickedText whether has appeared in the SelectedTags*/
 		$(".selectedTag").each(function() {
 			if ($(this).text() == (clickedText+'  ×')) {
-				TagHasClicked = 1;
-				
+				TagHasClicked = 1;				
 			};
 		});
 
 		/*If the Tag just clicked has not appeared, then display it*/
 		if (TagHasClicked == 0)	{
 			$(".selectedTags").append('<div class="selectedTag" title="点击删除该条件">'+$(this).text()+'  ×</div>');
-			var $w = $(".selectedTag:last").width();
-			var $h = $(".selectedTag:last").height();
-			var $w2 = $w+20;
-			var $h2 = $h+20;
-			$(".selectedTag:last").stop().animate({height:$h2,width:$w2},500,
-				function(){$(".selectedTag:last").stop().animate({height:$h,width:$w+3},500);});
+			// var $w = $(".selectedTag:last").width();
+			// var $h = $(".selectedTag:last").height();
+			// var $w2 = $w+20;
+			// var $h2 = $h+20;
+			// $(".selectedTag:last").stop().animate({height:$h2,width:$w2},500,
+			// 	function(){$(".selectedTag:last").stop().animate({height:$h,width:$w+3},500);});
 			
 		}
 		else alert(clickedText+"已经在筛选条件中了");
