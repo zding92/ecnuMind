906c29c82e2d5b46aaf310a0f923feba1c680bc3
ZHANG DING <zding92@126.com>
2015-08-23 14:24:03 +0800

    完成搜人界面中，【选择能力】之后判断是否重复，并加入筛选池

---
+++ Public/js/home/searchPeople.js
@@ -0,0 +1,25 @@
+/**
+ * This js is for the using of searchPeople Page
+ */
+$(document).ready(function() {
+	$("#searchPeoplePart2Row3 .searchPeopleTag").click(function() {
+		/* Act on the click event */
+
+		/* TagHasClicked = 0 means the tag has not been clicked; TagHasClicked = 1 means the tag has been clicked*/
+		var TagHasClicked = 0;
+
+		/*take out the text of the tag just clicked*/
+		var clickedText = $(this).text();
+
+		/*Judge each clickedText whether has appeared in the SelectedTags*/
+		$(".selectedTag").each(function() {
+			if ($(this).text() == (clickedText+'  ×')) {
+				TagHasClicked = 1;
+				alert(clickedText+"已经在筛选条件中了");
+			};
+		});
+
+		/*If the Tag just clicked has not appeared, then display it*/
+		if (TagHasClicked == 0)	$(".selectedTags").append('<div class="selectedTag" title="点击删除该条件">'+$(this).text()+'  ×</div>');
+	});
+});
