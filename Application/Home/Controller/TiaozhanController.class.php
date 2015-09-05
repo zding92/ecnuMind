7e0b4483466f73df8c6c175cb7da0ecf7747370f
YT <tian_yu92@yahoo.com>
2015-09-05 21:54:59 +0800

    挑战杯数据入库

--- Application/Home/Controller/TiaozhanController.class.php
+++ Application/Home/Controller/TiaozhanController.class.php
@@ -11,10 +11,18 @@ class TiaozhanController extends Controller {
 	 * 将挑战杯的数据录入数据库
 	 */
 	public function TiaozhanAddData(){
-		//将comp_Tiaozhan数据表实例化
-		$Data = M('Tiaozhan','comp_');		
+		// 将comp_Tiaozhan数据表实例化
+		$Data = M('ecnu_mind.tiaozhan_basic_info',null);		
 		// 根据表单提交的POST数据创建数据对象
 		$Data->create();
+		// 获取具体表格b1,b2 or b3
+		$bn = $Data->data()->type_selector;
+		$Data->add();
+		
+		$BnTable = M('ecnu_mind.tiaozhan_'.$bn,null);
+		$BnTable->create();
+		$BnTable->add();
+
 		// 根据条件保存修改的数据
 		if ($Data->save()) $this->ajaxReturn("var tiaozhanDataWri=true;","EVAL");; 	
 	}
