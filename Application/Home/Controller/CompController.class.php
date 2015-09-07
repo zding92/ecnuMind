6ada2391ca027d69dd34d362e2ef89f297981791
YT <tian_yu92@yahoo.com>
2015-09-07 23:01:18 +0800

    竞赛报名界面表格完善，竞赛信息入库

--- Application/Home/Controller/CompController.class.php
+++ Application/Home/Controller/CompController.class.php
@@ -6,4 +6,32 @@ class CompController extends Controller {
 		//显示__app__/home/comp/comp页面
 		$this->display();
 	}
+	
+	public function getCompInfo() {
+		$compModel = M('ecnu_mind.competition_info', null);
+		$allComp = $compModel->select();
+		$allComp = $this->checkDate($allComp);
+		$this->ajaxReturn(json_encode($allComp), "EVAL");
+	}
+	
+	private function checkDate($allComp) {
+		$today = date('Y-m-d', time());
+		$today = strtotime($today);
+		$result = array();
+		foreach ($allComp as $comp) {
+			$compStartDay = strtotime($comp['comp_apply_start_date']);
+			$compEndDay = strtotime($comp['comp_apply_end_date']);
+			if ($today < $compStartDay) {
+				$comp['apply_state'] = '未开始';
+				$comp['comp_template'] = "<a style='color:#aaa;margin:0px auto;'>报名未开始，请耐心等待</a>";
+			} elseif ($today > $compStartDay && $today < $compEndDay) {
+				$comp['apply_state'] = '正在报名(截止日期：'.$comp['comp_apply_end_date'].')';
+			} else {
+				$comp['apply_state'] = '报名结束';
+				$comp['comp_template'] = "<a style='color:#aaa;margin:0px auto;'>报名已经结束</a>";
+			}
+			$result[] = $comp;
+		}
+		return $result;
+	}
 }
--- Public/js/Comp/Comp.js
+++ Public/js/Comp/Comp.js
@@ -1,40 +1,11 @@
-/**
- * 取Comp.dat中的json数据，以在前台显示
- */
-$(document).ready(function () {
-	//alert('into the function');
-    $.ajax({
-        url: compJSON,
-        async: false,
-        success: function (result) {
-        	//alert('In to json ajax');
-        	//先删去表格中原始的行
-            $("tbody tr").detach();
-            eval(result);
-            //遍历所有的json数组元素
-            for (var i = 0; i < json_string.length; i++) { 
-            	//当json元素有link项时，从json数组中读取数据并append推送到前台页面中
-                if (json_string[i].link !== "")
-                    $("tbody").append("<tr><td data-title='ID'>" + json_string[i].no +
-                                        "</td><td data-title='Name'>" + json_string[i].name +
-                                        "</td><td data-title='Link'><a href='" + json_string[i].link + " 'target='_blank' >Link</a></td>" +
-                                        "</td><td data-title='Status'>" + json_string[i].status + "</td></tr>");
-              //当json元素无link项时，从json数组中读取数据并append推送到前台页面中
-                else
-                    $("tbody").append("<tr><td data-title='ID'>" + json_string[i].no +
-                                        "</td><td data-title='Name'>" + json_string[i].name +
-                                        "</td><td data-title='Link'>Link</td>" +
-                                        "</td><td data-title='Status'>" + json_string[i].status + "</td></tr>");
-
-            }
-
-        }
-    }); 
-  //当鼠标进入表格某行时，此行变蓝
-    $("tbody tr").mouseenter(function(){$(this).css("background-color","#B3E5FC");});
-  //当鼠标移出某行时，此行变白
-    $("tbody tr").mouseleave(function(){$(this).css("background-color","#fff");});
-  //配合iframe显示
-    parent.document.getElementById("Comp_frame").height = document.body.scrollHeight;
-});
+function starSorter(a, b) {
+	if (a.length > b.length) return 1;
+	if (a.length < b.length) return -1;
+	a = a.charAt(a.length-1);
+	b = b.charAt(b.length-1);
+	if (a == b) return 0;
+	if (a == '★' && b == '☆') return 1;
+	if (b == '★' && a == '☆') return -1;
+	return 0;
+}
 
