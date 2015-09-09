457c1f61459abc2522750923996d64f87afed9cc
YT <tian_yu92@yahoo.com>
2015-09-09 20:55:57 +0800

    【竞赛报名界面】 增加竞赛报名状态筛选

--- Public/js/Comp/Comp.js
+++ Public/js/Comp/Comp.js
@@ -16,10 +16,7 @@ $(function(){
 		height: 500
 	}).on('load-success.bs.table', function (e, data) {
 		table_filter.bootstrapTableFilter('enableFilter', 'comp_field');
-		
-		$('.dropdown-menu li a').mouseup(function(){
-			table_filter.bootstrapTableFilter('refresh');
-		});
+		table_filter.bootstrapTableFilter('enableFilter', 'apply_state');
     });
 	
 });
