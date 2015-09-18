4194ef5f58e658a3932b8fb067b9ed2b0c141b1f
ZHANG DING <zding92@126.com>
2015-09-19 00:04:59 +0800

    加入用户第一次使用的引导页面（前台）

---
+++ Application/Home/Controller/StepGuideController.class.php
@@ -0,0 +1,9 @@
+<?php
+namespace Home\Controller;
+use Home\Common\Controller\CommonController;
+class stepGuideController extends CommonController {
+	public function stepGuide(){
+		//显示__app__/home/stepGuide/stepGuide页面
+		$this->display();
+	}
+}
