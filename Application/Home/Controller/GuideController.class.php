d8f8f1ff2ca612c3ea2d1b6267995312195910e0
YT <tian_yu92@yahoo.com>
2015-09-19 14:21:58 +0800

    引导后台

--- Application/Home/Controller/GuideController.class.php
+++ Application/Home/Controller/GuideController.class.php
@@ -7,10 +7,21 @@ class GuideController extends CommonController {
 		$infoModel = M('ecnu_mind.user_info');
 		// 检查哪些列为空
 		$uid = session('userid');
-		$infoData = $infoModel->field('id,username,password', true)->find($uid);
+		$infoData = $infoModel->field('id,username,password,brief', true)->find($uid);
 		$startStep['stepGuideInProgress'] = $infoData['info_complete'];
 		$startStep['stepGuideNum'] = count($infoData) - 1;
 		$this->assign($startStep);
 		$this->display();
+	}
+	
+	public function submitItem() {
+		$infoModel = D('info');
+		
+		if (!$infoModel->create()) {
+			$this->ajaxReturn($infoModel->getError(), 'EVAL');
+		} else {
+			$infoModel->where('id='.session('userid'))->save();
+			$this->ajaxReturn('success', 'EVAL');
+		}
 	}
 }
