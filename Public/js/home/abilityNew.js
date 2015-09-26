af0a5da94ce1c021bc8c4500ec719a2d338fc096
ZHANG DING <zding92@126.com>
2015-09-26 19:35:21 +0800

    个人能力框样式初步完成

--- Public/js/home/abilityNew.js
+++ Public/js/home/abilityNew.js
@@ -1,193 +1,65 @@
-﻿$.fn.sjld = function(shenfen,chengshi,quyu){
-	var sfp = shenfen+' p'
-	var csp = chengshi+' p'
-	var qyp = quyu+' p'
-	var sfs = shenfen+' .m_zlxg2'
-	var css = chengshi+' .m_zlxg2'
-	var qys = quyu+' .m_zlxg2'
-	var sfli = shenfen+' ul li'
-	var csli = chengshi+' ul li'
-	var qyli = quyu+' ul li'
-	$('.m_zlxg').click(function(){
-		$(this).find('.m_zlxg2').slideDown(200);
-	})
-	$('.m_zlxg').mouseleave(function(){
-		$(this).find('.m_zlxg2').slideUp(200);
-	})
-	var sfgsmr = provinceList;
-	var csgsmr = provinceList[0].cityList;
-	var qygsmr = provinceList[0].cityList[0].areaList;
-	var kuandu = new Array();
-	
-	
-	$(sfp).text(sfgsmr[0].name);
-	$(csp).text(csgsmr[0].name);
-	$(qyp).text(qygsmr[0]);
-	//默认城市
-	for(a=0;a<sfgsmr.length;a++){
-		var sfmcmr = sfgsmr[a].name;
-		var sfnrmr = "<li>"+sfmcmr+"</li>";
-		$(shenfen).find('ul').append(sfnrmr);
-	}
-	for(b=0;b<csgsmr.length;b++){
-		var csmcmr = csgsmr[b].name;
-		
-		var csnrmr = "<li>"+csmcmr+"</li>";
-		$(chengshi).find('ul').append(csnrmr);
-		kuandu[b] =csmcmr.length*14+20;
-	}
-	for(c=0;c<qygsmr.length;c++){
-		var qymcmr = qygsmr[c];
-		var qynrmr = "<li>"+qymcmr+"</li>";
-		$(quyu).find('ul').append(qynrmr);
-	}
-	Array.max=function(array)
-		{
-    		return Math.max.apply(Math,array);
-		}
-	var max_kd = Array.max(kuandu); 
-		if(max_kd<118){
-			$(css).width('118px');
-		}
-			else{
-					$(css).width(max_kd);	
-			}
-	
-/*---------------------------------------------------------------------*/
-
-	$(sfli).click(function(){
-		var dqsf = $(this).text();
-		$(shenfen).find('p').text(dqsf);
-		$(shenfen).find('p').attr('title',dqsf);
-		var sfnum = $(this).index();
-		
-	var csgs = provinceList[sfnum].cityList;
-	var csgs2 = provinceList[sfnum].cityList[0].areaList;
-	$(chengshi).find('ul').text('');
-	var kuandu = new Array();
-	for(i=0;i<csgs.length;i++){
-		var csmc = csgs[i].name;
-		var csnr = "<li>"+csmc+"</li>";
-		$(chengshi).find('ul').append(csnr);
-		kuandu[i] =csmc.length*14+20;
-	}
-Array.max=function(array)
+﻿// 纯JS省市区三级联动
+// 2011-11-30 by http://www.cnblogs.com/zjfree
+var addressInit = function(_cmbProvince, _cmbCity, _cmbArea, defaultProvince, defaultCity, defaultArea)
 {
-    return Math.max.apply(Math,array);
+	var cmbProvince = document.getElementById(_cmbProvince);
+	var cmbCity = document.getElementById(_cmbCity);
+	var cmbArea = document.getElementById(_cmbArea);
+	
+	function cmbSelect(cmb, str)
+	{
+		for(var i=0; i<cmb.options.length; i++)
+		{
+			if(cmb.options[i].value == str)
+			{
+				cmb.selectedIndex = i;
+				return;
+			}
+		}
+	}
+	function cmbAddOption(cmb, str, obj)
+	{
+		var option = document.createElement("OPTION");
+		cmb.options.add(option);
+		option.innerHTML = str;
+		option.value = str;
+		option.obj = obj;
+	}
+	
+	function changeCity()
+	{
+		cmbArea.options.length = 0;
+		if(cmbCity.selectedIndex == -1)return;
+		var item = cmbCity.options[cmbCity.selectedIndex].obj;
+		for(var i=0; i<item.areaList.length; i++)
+		{
+			cmbAddOption(cmbArea, item.areaList[i], null);
+		}
+		cmbSelect(cmbArea, defaultArea);
+	}
+	function changeProvince()
+	{
+		cmbCity.options.length = 0;
+		cmbCity.onchange = null;
+		if(cmbProvince.selectedIndex == -1)return;
+		var item = cmbProvince.options[cmbProvince.selectedIndex].obj;
+		for(var i=0; i<item.cityList.length; i++)
+		{
+			cmbAddOption(cmbCity, item.cityList[i].name, item.cityList[i]);
+		}
+		cmbSelect(cmbCity, defaultCity);
+		changeCity();
+		cmbCity.onchange = changeCity;
+	}
+	
+	for(var i=0; i<provinceList.length; i++)
+	{
+		cmbAddOption(cmbProvince, provinceList[i].name, provinceList[i]);
+	}
+	cmbSelect(cmbProvince, defaultProvince);
+	changeProvince();
+	cmbProvince.onchange = changeProvince;
 }
-var max_kd = Array.max(kuandu); 
-if(max_kd<91){
-	$(css).width('91px');
-	}
-	else{
-	$(css).width(max_kd);	
-	}
-	var qygsdqmr = provinceList[sfnum].cityList[0].areaList;
-	$(quyu).find('ul').text('');
-	for(j=0;j<qygsdqmr.length;j++){
-		var qymc = qygsdqmr[j];
-		var qynr = "<li>"+qymc+"</li>";
-		$(quyu).find('ul').append(qynr);
-	}		
-	$(csp).text(csgs[0].name);
-	$(qyp).text(csgs2[0]);
-	$('#sfdq_num').val(sfnum);
-
-/*------------------*/
-	$(csli).click(function(){
-		var dqcs = $(this).text();
-		var dqsf_num = $('#sfdq_num').val();
-		if(dqsf_num==""){
-			dqsf_num=0;
-			}
-			else{
-			var dqsf_num = $('#sfdq_num').val();
-			}
-		$(chengshi).find('p').text(dqcs);
-		$(chengshi).find('p').attr('title',dqcs);
-		var csnum = $(this).index();
-	var qygs = provinceList[dqsf_num].cityList[csnum].areaList;
-	$(quyu).find('ul').text('');
-	for(j=0;j<qygs.length;j++){
-		var qymc = qygs[j];
-		var qynr = "<li>"+qymc+"</li>";
-		$(quyu).find('ul').append(qynr);
-	}
-	
-$(qyp).text(qygs[0]);
-	$('#csdq_num').val(csnum);
-	
-	$(this).parents('.m_zlxg2').width(kuandu);
-	$(qyli).click(function(){
-	var dqqy = $(this).text();
-		$(quyu).find('p').text(dqqy);
-		$(quyu).find('p').attr('title',dqqy);
-			
-})//区级
-	})	//市级
-/*------------------*/	
-$(qyli).click(function(){
-	var dqqy = $(this).text();
-		$(quyu).find('p').text(dqqy);
-		$(quyu).find('p').attr('title',dqqy);
-			
-})//区级
-
-
-		})//省级
-/*---------------------------------------------------------------------*/		
-		
-		
-		
-	$(csli).click(function(){
-		var dqcs = $(this).text();
-		var dqsf_num = $('#sfdq_num').val();
-		if(dqsf_num==""){
-			dqsf_num=0;
-			}
-			else{
-			var dqsf_num = $('#sfdq_num').val();
-			}
-		$(chengshi).find('p').text(dqcs);
-		$(chengshi).find('p').attr('title',dqcs);
-		var csnum = $(this).index();
-	var qygs = provinceList[dqsf_num].cityList[csnum].areaList;
-	$(quyu).find('ul').text('');
-	for(j=0;j<qygs.length;j++){
-		var qymc = qygs[j];
-		var qynr = "<li>"+qymc+"</li>";
-		$(quyu).find('ul').append(qynr);
-	}
-$(qyp).text(qygs[0]);
-	$('#csdq_num').val(csnum);
-	/*------------------*/
-	$(qyli).click(function(){
-	var dqqy = $(this).text();
-		$(quyu).find('p').text(dqqy);
-		$(quyu).find('p').attr('title',dqqy);
-
-			
-})//区级
-	})	//市级
-/*---------------------------------------------------------------------*/	
-	
-$(qyli).click(function(){
-	var dqqy = $(this).text();
-		$(quyu).find('p').text(dqqy);
-		$(quyu).find('p').attr('title',dqqy);
-
-			
-})//区级
-
-/*---------------------------------------------------------------------*/
-$('.m_zlxg').click(function(){
-	$('#sfdq_tj').val($(sfp).text());
-	$('#csdq_tj').val($(csp).text());
-	$('#qydq_tj').val($(qyp).text());
-	})//表单传值获取
-
-}
-
 
 var provinceList = [
 {name:'北京', cityList:[		   
@@ -598,12 +470,9 @@ var provinceList = [
 {name:'省直辖县级行政单位', areaList:['五指山市','琼海市','儋州市','文昌市','万宁市','东方市','定安县','屯昌县','澄迈县','临高县','白沙黎族自治县','昌江黎族自治县','乐东黎族自治县','陵水黎族自治县','保亭黎族苗族自治县','琼中黎族苗族自治县','西沙群岛','南沙群岛','中沙群岛的岛礁及其海域']}
 ]},
 {name:'台湾', cityList:[
-{name:'台湾', areaList:['台湾']}
 ]},
 {name:'香港', cityList:[
-{name:'香港', areaList:['香港']}
 ]},
 {name:'澳门', cityList:[
-{name:'澳门', areaList:['澳门']}
 ]}
 ];
