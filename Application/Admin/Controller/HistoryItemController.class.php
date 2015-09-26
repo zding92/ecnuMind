b927cf3fc45a087f8c7ff4919416424647d5aac8
ZHANG DING <zding92@126.com>
2015-09-26 14:40:50 +0800

    添加HistoryItem.js

--- Application/Admin/Controller/HistoryItemController.class.php
+++ Application/Admin/Controller/HistoryItemController.class.php
@@ -2,8 +2,30 @@
 namespace Admin\Controller;
 use Think\Controller;
 use Admin\Common\Controller\CommonController;
+
 class HistoryItemController extends CommonController {
     public function Historyitem(){
     	$this->display();
-  }
+  	}
+  	
+  	public function showAllHistoryItem(){
+  		$compItemModel = M('ecnu_mind.competition_main', null);
+		$compInfoModel = M('ecnu_mind.competition_info', null);
+		
+		//allHistoryItem为competition_main 表格中的所有行，取"comp_item_id","comp_type_id"列的二维数组
+		$allHistoryItem = $compItemModel -> field("comp_item_id","comp_type_id") -> select();
+		
+		//遍历$allHistoryItem的每一行，每一行成为一个一维数组$eachHistoryItem
+		foreach ($allHistoryItem as $eachHistoryItem){
+			
+			// 获得该项竞赛的基本信息,在competition_info表中，找到对应的comp_type_id的行，取出其conp_name和comp_template
+			$compinfo =	$compItemModel
+			->where('comp_id='.$eachHistoryItem['comp_type_id'])
+			->field("comp_name, comp_template")
+			->find();
+			
+			// 根据模板名称和竞赛报名ID获得详细信息
+			$compDetailModel = M('ecnu_mind.'.$compinfo['comp_template'].'_info',null);
+		}
+  	}
 }
---
+++ Public/js/HistoryItem/HistoryItem.js
@@ -0,0 +1,38 @@
+/**
+ * HistoryItem管理员界面中历史项目页面使用的js
+ */
+function starSorter(a, b) {
+	if (a.length > b.length) return 1;
+	if (a.length < b.length) return -1;
+	a = a.charAt(a.length-1);
+	b = b.charAt(b.length-1);
+	if (a == b) return 0;
+	if (a == '★' && b == '☆') return 1;
+	if (b == '★' && a == '☆') return -1;
+	return 0;
+}
+
+$(function(){
+	$table = $("#comp-table").bootstrapTable({
+		striped: true,
+		pagination: true,
+		height: 600,
+		pageSize: 20
+	}).on('load-success.bs.table', function (e, data) {
+		table_filter.bootstrapTableFilter('enableFilter', 'comp_field');
+		table_filter.bootstrapTableFilter('enableFilter', 'apply_state');
+		// 将第四列隐藏（参数：切换列显示状态（toggleColumn）, 第N列(第4列)，隐藏，更新表）
+		$table.bootstrapTable('toggleColumn', 4, false, true);
+		
+		//点击下拉选择的整体，能够选中勾选框
+		$('.dropdown-menu li a :input').click(function(e){
+			if($(this).prop('checked')){
+				$(this).prop('checked',false);
+			} else {
+				$(this).prop('checked',true);
+			}
+		});
+    });
+	
+});
+
