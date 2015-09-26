6279984a38508521bc9eff111bd3878661a95331
YT <tian_yu92@yahoo.com>
2015-09-26 23:52:57 +0800

    调整competition_main表逻辑

--- Application/Admin/Controller/HistoryItemController.class.php
+++ Application/Admin/Controller/HistoryItemController.class.php
@@ -43,13 +43,14 @@ class HistoryItemController extends CommonController {
 			
 			
 		
-		// $allCompItem为competition_main 表格中的所有行，取"comp_item_id,comp_type_id,apply_date,comp_state,comp_prize"列的二维数组
+		// $allCompItem为competition_main 表格中的所有行，
+		// 取"comp_item_id,comp_item_name,author1_name,owner_academy,comp_type_id,apply_date,comp_state,comp_prize"列的二维数组
 		// 并且由前台分页工具决定载入的数量。
 		$allCompItem = $compItemModel
-								 ->where($condition)
-								 ->limit("'".I('get.offset').",".I('get.limit')."'")
-								 ->field('comp_participant_id',true)
-								 ->select();
+					   ->where($condition)
+					   ->limit("'".I('get.offset').",".I('get.limit')."'")
+					   ->field('comp_participant_id',true)
+					   ->select();
 		
 		//返回二维数组的行数
 		$i = 0;		
@@ -59,41 +60,17 @@ class HistoryItemController extends CommonController {
 			
 			// 获得该项竞赛的基本信息,在competition_info表中，找到对应的comp_type_id的行，取出其conp_name和comp_template
 			$compinfo =	$compInfoModel
-								->where('comp_id='.$eachCompItem['comp_type_id'])
-								->field("comp_name,comp_template")
-								->find();
-					
-			// 竞赛模板名+_info为数据库表名。
-			$compModelName = $compinfo['comp_template'].'_info';
-			
-			// 将所有竞赛报名信息表实例化成数组,如果数组中包含该表对象，则跳过实例化。
-			if (!isset($compModels[$compModelName])) {
-				$compModels[$compModelName] = M($compModelName);
-			}
-			
-			// 根据表名获取其表对象。
-			$compDetailModel = $compModels[$compModelName];			
-			
-			//$returnItemInfo为在对应的竞赛细节表中找到的对应的item_id的行
-			//提取项目名称、第一作者
-			$compDetailItem = $compDetailModel 
-								-> where($eachCompItem)
-								-> field("comp_item_name,author1_id")
-								-> find();
-			
-			//获取第一作者的姓名和学院
-			$author1DetailInfo = $userDetailModel
-									-> where('student_id='.$compDetailItem['author1_id'])
-									-> field('name,academy')  
-									-> find();
+						->where('comp_id='.$eachCompItem['comp_type_id'])
+						->field("comp_name")
+						->find();
 						
 			//$returnToFront为返回到前台的二维数组变量
 			$returnItemInfo['comp_name'] = $compinfo['comp_name'];
 			$returnItemInfo['comp_prize'] = $eachCompItem['comp_prize'];
-			$returnItemInfo['comp_item_name'] = $compDetailItem['comp_item_name'];
+			$returnItemInfo['comp_item_name'] = $eachCompItem['comp_item_name'];
 			$returnItemInfo['comp_date'] = substr($eachCompItem['apply_date'],0,4).'年';
-			$returnItemInfo['comp_author1'] = $author1DetailInfo['name'];
-			$returnItemInfo['apply_department'] = $author1DetailInfo['academy'];
+			$returnItemInfo['comp_author1'] = $eachCompItem['author1_name'];
+			$returnItemInfo['apply_department'] = $eachCompItem['owner_academy'];
 			
 			$returnToFront['rows'][$i] = $returnItemInfo;
 			
