27f0268df5e0bffe0ef4a3d5f3cc4fb519559524
YT <tian_yu92@yahoo.com>
2015-09-27 18:36:10 +0800

    【历史竞赛信息】使用文件缓存，一次性全部载入到前台，代替分页ajax请求以保证效率和代码可读性。

--- Application/Admin/Controller/HistoryItemController.class.php
+++ Application/Admin/Controller/HistoryItemController.class.php
@@ -18,46 +18,48 @@ class HistoryItemController extends CommonController {
 		//建立作者user_custom表的model
 		$userDetailModel = M('ecnu_mind.user_custom');
 		
-		if (null === S()) {
+		// 如果不存在已结束竞赛信息缓存
+		if (!S("finish_comps")) {
 			
-		}
-		
-		// 检索条件
-		$condition['comp_state'] = '已结束';			
-		
-		// $allCompItem为competition_main 表格中的所有行，
-		// 取"comp_item_id,comp_item_name,author1_name,owner_academy,comp_type_id,apply_date,comp_state,comp_prize"列的二维数组
-		// 并且由前台分页工具决定载入的数量。
-		$allCompItem = $compItemModel
-					   ->where($condition)
-					   ->limit("'".I('get.offset').",".I('get.limit')."'")
-					   ->field('comp_participant_id',true)
-					   ->select();
-		
-		//返回二维数组的行数
-		$i = 0;		
-		
-		//遍历$allHistoryItem的每一行，每一行成为一个一维数组$eachHistoryItem
-		foreach ($allCompItem as $eachCompItem) {
+			// 检索条件
+			$condition['comp_state'] = '已结束';			
 			
-			// 获得该项竞赛的基本信息,在competition_info表中，找到对应的comp_type_id的行，取出其conp_name和comp_template
-			$compinfo =	$compInfoModel
-						->where('comp_id='.$eachCompItem['comp_type_id'])
-						->field("comp_name")
-						->find();
-						
-			//$returnToFront为返回到前台的二维数组变量
-			$returnItemInfo['comp_name'] = $compinfo['comp_name'];
-			$returnItemInfo['comp_prize'] = $eachCompItem['comp_prize'];
-			$returnItemInfo['comp_item_name'] = $eachCompItem['comp_item_name'];
-			$returnItemInfo['comp_date'] = substr($eachCompItem['apply_date'],0,4).'年';
-			$returnItemInfo['comp_author1'] = $eachCompItem['author1_name'];
-			$returnItemInfo['apply_department'] = $eachCompItem['owner_academy'];
+			// $allCompItem为competition_main 表格中的所有行，
+			// 取"comp_item_id,comp_item_name,author1_name,owner_academy,comp_type_id,apply_date,comp_state,comp_prize"列的二维数组
+			// 并且由前台分页工具决定载入的数量。
+			$allCompItem = $compItemModel
+						   ->where($condition)
+						   ->limit("'".I('get.offset').",".I('get.limit')."'")
+						   ->field('comp_participant_id',true)
+						   ->select();
 			
-			$returnToFront['rows'][$i] = $returnItemInfo;
+			//返回二维数组的行数
+			$i = 0;		
 			
-			$i++;
-		}
-		$this->ajaxReturn(json_encode($returnToFront),'EVAL');
+			//遍历$allHistoryItem的每一行，每一行成为一个一维数组$eachHistoryItem
+			foreach ($allCompItem as $eachCompItem) {
+				
+				// 获得该项竞赛的基本信息,在competition_info表中，找到对应的comp_type_id的行，取出其conp_name和comp_template
+				$compinfo =	$compInfoModel
+							->where('comp_id='.$eachCompItem['comp_type_id'])
+							->field("comp_name")
+							->find();
+							
+				//$returnToFront为返回到前台的二维数组变量
+				$returnItemInfo['comp_name'] = $compinfo['comp_name'];
+				$returnItemInfo['comp_prize'] = $eachCompItem['comp_prize'];
+				$returnItemInfo['comp_item_name'] = $eachCompItem['comp_item_name'];
+				$returnItemInfo['comp_date'] = substr($eachCompItem['apply_date'],0,4).'年';
+				$returnItemInfo['comp_author1'] = $eachCompItem['author1_name'];
+				$returnItemInfo['apply_department'] = $eachCompItem['owner_academy'];
+				
+				$returnToFront[$i] = $returnItemInfo;
+				
+				$i++;
+			}
+			S('finish_comps', json_encode($returnToFront), array('type'=>'file','expire'=>600));
+		} 
+		
+		$this->ajaxReturn(S('finish_comps'),'EVAL');
   	}
 }
