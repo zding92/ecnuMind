47b69ec4defd013d84aafdf3d2528b1d71d258e4
ZHANG DING <zding92@126.com>
2015-09-28 17:17:04 +0800

    在管理员界面中，历史项目页面中，添加院系筛选条件的显示功能

--- Public/js/CurrentComp.js
+++ Public/js/CurrentComp.js
@@ -1,4 +1,7 @@
 /**
+ *管理员界面中当前比赛页面使用的js
+ */
+/**
  * HistoryItem管理员界面中历史项目页面使用的js
  */
 function starSorter(a, b) {
@@ -16,6 +19,7 @@ $(function(){
 	var initialFlag = true;
 	$table = $("#comp-table").bootstrapTable({
 		striped: true,
+		sidePagination: 'server',
 		pagination: true,
 		height: 600,
 		pageSize: 20
@@ -23,10 +27,6 @@ $(function(){
 		if (initialFlag) {
 			table_filter.bootstrapTableFilter('enableFilter', 'comp_name');
 			table_filter.bootstrapTableFilter('enableFilter', 'comp_date');
-			
-			// 如果管理员是校级管理员，显示院系过滤框
-			if (admin == 'all') 
-				table_filter.bootstrapTableFilter('enableFilter', 'apply_department');
 			
 			//点击下拉选择的整体，能够选中勾选框
 			$('.dropdown-menu li a :input').click(function(e){
@@ -37,20 +37,15 @@ $(function(){
 				}
 			});
 			
-			//筛选框点击，显示筛选条件
 			$('.dropdown-menu li a').click(function(){
-				//取出点击的筛选条件类型
 				var filterType = $(this).parent().parent().parent().attr('data-filter-field');
-				//取出点击的筛选条件
 				var filterText = $(this).parent().text();
+				//alert(filterType+'='+filterText);
 				
-				//如果先前此checkbox未被checked，即要被checked
 				if(!$(this).children('input').prop('checked')){
-					//在于筛选条件类型同名id的显示框中，显示刚才点击的筛选内容
 					$('#'+filterType).append(filterText+';');
 				}
 				else{
-					//在于筛选条件类型同名id的显示框中，删除刚才点击的筛选内容
 					var currentFilterText = $('#'+filterType).text();
 					var afterFilterText = currentFilterText.replace(filterText+';','');
 					$('#'+filterType).text(afterFilterText);
